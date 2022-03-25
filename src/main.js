17d32ba6e511f9f7ee2cd92a06bff3275cff6274
neuefische <neuefische@Nimas-MacBook-Air.fritz.box>
2022-03-25 19:05:52 +0100

    Filter Box and JS Functions edited

--- src/main.js
+++ src/main.js
@@ -7,8 +7,6 @@ const urls = Array(numPages)
 
 const promises = urls.map(url => fetch(url).then(res => res.json()))
 
-
-
 const loadDataButtonElement = document.querySelector('.header-btn')
 const cardContainerElement = document.querySelector('.cards-container')
 
@@ -25,81 +23,41 @@ Promise
 
 function renderCharacter(character) {
     const cardContainerElement = document.querySelector('.cards-container')
-    const filterElement=document.querySelector('[list="character-state"]')
-    if(filterElement.value===''){
-  
-  const newCardElement = document.createElement('article')
-  newCardElement.classList.add('cards')
-  cardContainerElement.append(newCardElement)
-
-  const newImageElement = document.createElement('img')
-  newImageElement.setAttribute('src', character.image)
-  newCardElement.append(newImageElement)
-
-  const paragraphElement = document.createElement('p')
-  paragraphElement.textContent = character.name;
-  newCardElement.append(paragraphElement)
-
+    const filterElement=document.querySelector("#character-state")
+ if(filterElement.value==='All'){
+    addCardElements(character);
     }
      if(filterElement.value==='Alive'){
         if(character.status==='Alive'){
-        
-            const newCardElement = document.createElement('article')
-            newCardElement.classList.add('cards')
-            cardContainerElement.append(newCardElement)
-          
-            const newImageElement = document.createElement('img')
-            newImageElement.setAttribute('src', character.image)
-            newCardElement.append(newImageElement)
-          
-            const paragraphElement = document.createElement('p')
-            paragraphElement.textContent = character.name;
-            newCardElement.append(paragraphElement)
-
-            const paragraphStatusElement = document.createElement('p')
-            paragraphStatusElement.textContent = character.status;
-            newCardElement.append(paragraphStatusElement)
+        addCardElements(character);
         }
         
           }
           if(filterElement.value==='Dead'){
         if(character.status==='Dead'){
-        
-            const newCardElement = document.createElement('article')
-            newCardElement.classList.add('cards')
-            cardContainerElement.append(newCardElement)
-          
-            const newImageElement = document.createElement('img')
-            newImageElement.setAttribute('src', character.image)
-            newCardElement.append(newImageElement)
-          
-            const paragraphElement = document.createElement('p')
-            paragraphElement.textContent = character.name;
-            newCardElement.append(paragraphElement)
-
-            const paragraphStatusElement = document.createElement('p')
-            paragraphStatusElement.textContent = character.status;
-            newCardElement.append(paragraphStatusElement)
+            addCardElements(character);
         }
           }
           if(filterElement.value==='Unknown'){
             if(character.status==='unknown'){
-            
-                const newCardElement = document.createElement('article')
-                newCardElement.classList.add('cards')
-                cardContainerElement.append(newCardElement)
-              
-                const newImageElement = document.createElement('img')
-                newImageElement.setAttribute('src', character.image)
-                newCardElement.append(newImageElement)
-              
-                const paragraphElement = document.createElement('p')
-                paragraphElement.textContent = character.name;
-                newCardElement.append(paragraphElement)
-    
-                const paragraphStatusElement = document.createElement('p')
-                paragraphStatusElement.textContent = character.status;
-                newCardElement.append(paragraphStatusElement)
+                addCardElements(character);
             }
               }
+}
+function addCardElements(character){
+    const newCardElement = document.createElement('article')
+    newCardElement.classList.add('cards')
+    cardContainerElement.append(newCardElement)
+  
+    const newImageElement = document.createElement('img')
+    newImageElement.setAttribute('src', character.image)
+    newCardElement.append(newImageElement)
+  
+    const paragraphElement = document.createElement('p')
+    paragraphElement.textContent = character.name;
+    newCardElement.append(paragraphElement)
+
+    const paragraphStatusElement = document.createElement('p')
+    paragraphStatusElement.textContent = character.status;
+    newCardElement.append(paragraphStatusElement)
 }
